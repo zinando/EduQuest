a093f09043b3ea0472e42e254e8d6726a956cb0d
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-08-28 16:45:44 +0100

    Update Login.jsx

--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -10,9 +10,6 @@ export default function SignUp() {
       <div className="container-fluid content">
         <div className='row'>
           <div className="col-lg-6 col-md-6 d-none d-md-block image-container"></div>
-
-
-
           <div className="col-lg-6 col-md-7 form-container">
             <Link to="/SignUp" className='mb-3 logo'><h2>Edu<span className='quest'>Quest</span><span className='yellow'>.</span></h2></Link>
             <form action="#">
@@ -28,9 +25,9 @@ export default function SignUp() {
                 />
               </div>
 
-              
-            
-        
+
+
+
               <div className="form-group mt-5 d-flex align-items-center">
                 <label htmlFor="name" className='form2'>
                   <Unicons.UilLock color="#e97464" size="20" />

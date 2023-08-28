01bb7abf2d496448e6f01a3fe127521e98f786b2
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-08-28 15:22:58 +0100

    Create SideBar.css

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import SignUp from './pages/Signup/SignUp'
 import Login from './pages/Login/Login'
+import Forgot from './pages/Login/Forgot'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -14,7 +15,8 @@ export default function App() {
       <Router>
         <Routes>
           <Route path="/signup" element={<SignUp/>} />
-          <Route path="/login" element={<Login/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgot" element={<Forgot />} />
         </Routes>
       </Router>
         

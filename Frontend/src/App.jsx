d3f8f882308e99a7a11e208f8b4fa5127b82e004
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-08-29 16:28:14 +0100

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,8 +2,6 @@ import SignUp from './pages/Signup/SignUp'
 import Login from './pages/Login/Login'
 import Forgot from './pages/Login/Forgot'
 import Error from './pages/Login/error'
-import SideBar from './pages/Dashboard/SideBar/SideBar'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -20,7 +18,7 @@ export default function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/forgot" element={<Forgot />} />
           <Route path="/error" element={< Error />} />
-          <Route path="/sidebar" element={< SideBar />} />
+          
         </Routes>
       </Router>
         

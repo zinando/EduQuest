37373e7eef61f5436c910f53e6002ee11073c836
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-02 10:28:42 +0100

    Update Login.jsx

--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -1,76 +1,71 @@
+
+import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Login.css'
 import * as Unicons from '@iconscout/react-unicons';
 import { Link } from 'react-router-dom';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-export default function SignUp() {
+function SignUp() {
   return (
     <>
-
       <div className="container-fluid content">
         <div className='row'>
           <div className="col-lg-6 col-md-6 d-none d-md-block image-container"></div>
           <div className="col-lg-6 col-md-7 form-container">
             <Link to="/error" className='mb-3 logo'><h2>Edu<span className='quest'>Quest</span><span className='yellow'>.</span></h2></Link>
-            <form action="#">
-              <div className="form-group mb-3 d-flex align-items-center">
-                <label htmlFor="name" className='form2'>
+            <Form>
+              <Form.Group className="mb-3 d-flex align-items-center">
+                <Form.Label htmlFor="name" className='form2'>
                   <Unicons.UilUser color="#e97464" size="20" />
-                </label>
-                <input
+                </Form.Label>
+                <Form.Control
                   type="text"
-                  className="form-control"
                   placeholder="Enter Username"
                   required=""
                 />
-              </div>
+              </Form.Group>
 
-
-
-
-              <div className="form-group mt-5 d-flex align-items-center">
-                <label htmlFor="name" className='form2'>
+              <Form.Group className="mb-3 d-flex align-items-center">
+                <Form.Label htmlFor="name" className='form2'>
                   <Unicons.UilLock color="#e97464" size="20" />
-                </label>
-                <input
+                </Form.Label>
+                <Form.Control
                   type="password"
-                  className="form-control"
                   placeholder="Enter password"
                   required=""
                 />
+              </Form.Group>
 
-              </div>
+              <Form.Group as={Row} className="mb-3" controlId="formHorizontalCheck">
+                <Col sm={{ span: 5, offset: 0 }}>
+                  <Form.Check label="Remember me" />
+                </Col>
+              </Form.Group>
 
-
-              <div className="form-check ">
-                <input type="checkbox" className="form-check-input" />
-                <label className="form-check-label">
-                  Remember me
-                </label>
-              </div>
               <div className="form-group mb-3">
-                <input
+                <button
                   type="submit"
                   className="btn button"
-                  defaultValue="Register Now"
-                />
+                >
+                  Register Now
+                </button>
               </div>
+
               <div className="text">
-                <h3 >
+                <h3>
                   Already have an account? <Link to="/SignUp">Sign Up</Link>
                 </h3>
-                <h3 >
+                <h3>
                   <Link to="/Forgot">Forgot your password?</Link>
                 </h3>
               </div>
-            </form>
+            </Form>
           </div>
         </div>
       </div>
-
-
-
     </>
-
-  )
+  );
 }
+
+export default SignUp;

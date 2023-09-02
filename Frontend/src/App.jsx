8176dd50e8ed921e3425f3bf344adb8b36272c2c
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-02 21:58:48 +0100

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,28 +6,31 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SideBar from './layout/Sidebar/SideBar'
 import Home from './pages/Dashboard/Home'
+import Schedule from './pages/Schedule/Schedule'
+
 
 export default function App() {
-  
+
 
   return (
     <>
       <div>
-      <Router>
-        <Routes>
-          <Route path="/signup" element={<SignUp/>} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgot" element={<Forgot />} />
+        <Router>
+          <Routes>
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forgot" element={<Forgot />} />
             <Route path="/error" element={< Error />} />
             <Route path="/sidebar" element={< SideBar />} />
             <Route path="/home" element={< Home />} />
-        </Routes>
+            <Route path="/schedule" element={< Schedule />} />
+          </Routes>
         </Router>
-        
-        
-            
-            
-        
+
+
+
+
+
       </div>
     </>
   )

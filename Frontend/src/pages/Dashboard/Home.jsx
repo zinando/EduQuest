bdd01076eb2a9bb06e2cac4b8202efa744a60a77
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-02 11:13:00 +0100

    Update Home.jsx

--- Frontend/src/pages/Dashboard/Home.jsx
+++ Frontend/src/pages/Dashboard/Home.jsx
@@ -1,22 +1,23 @@
-
-import './Home.css'; 
+import SideBar from '../../layout/Sidebar/SideBar';
+import './Home.css';
 
 export default function Home() {
   return (
-    <div className="home-container">
-      <h1 className="home-header">Welcome to the Dashboard Home Page</h1>
-      <p>This is the main overview of your dashboard.</p>
-      <div className="data-summary">
-        <h2>Data Summary</h2>
-        <p>Here &apos s a quick summary of your data:</p>
-        <ul>
-          <li>Total Users: 1000</li>
-          <li>Total Orders: 500</li>
-          <li>Revenue: $50,000</li>
-        </ul>
+    <div className="hm-container">
+      <SideBar />
+      <div className="home-container">
+        <h1 className="home-header">Welcome to the Dashboard Home Page</h1>
+        <p>This is the main overview of your dashboard.</p>
+        <div className="data-summary">
+          <h2>Data Summary</h2>
+          <p>Here&apos;s a quick summary of your data:</p>
+          <ul>
+            <li>Total Users: 1000</li>
+            <li>Total Orders: 500</li>
+            <li>Revenue: $50,000</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
 }
-
-

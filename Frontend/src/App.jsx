e6e947555d9c3b639fdee85fabf7245b5b993e1b
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-02 10:28:19 +0100

    Added route to dashboard

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,24 +4,31 @@ import Forgot from './pages/Login/Forgot'
 import Error from './pages/Login/error'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-
+import SideBar from './layout/Sidebar/SideBar'
+import Home from './pages/Dashboard/Home'
 
 export default function App() {
   
 
   return (
     <>
+      <div>
       <Router>
         <Routes>
           <Route path="/signup" element={<SignUp/>} />
           <Route path="/login" element={<Login />} />
           <Route path="/forgot" element={<Forgot />} />
-          <Route path="/error" element={< Error />} />
-          
+            <Route path="/error" element={< Error />} />
+            <Route path="/sidebar" element={< SideBar />} />
+            <Route path="/home" element={< Home />} />
         </Routes>
-      </Router>
+        </Router>
         
+        
+            
+            
+        
+      </div>
     </>
   )
 }

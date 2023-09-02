3b1680db1117028f6ab26b849be2b11870b082b9
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-02 10:28:38 +0100

    Create Home.jsx

---
+++ Frontend/src/pages/Dashboard/Home.jsx
@@ -0,0 +1,22 @@
+
+import './Home.css'; 
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1 className="home-header">Welcome to the Dashboard Home Page</h1>
+      <p>This is the main overview of your dashboard.</p>
+      <div className="data-summary">
+        <h2>Data Summary</h2>
+        <p>Here &apos s a quick summary of your data:</p>
+        <ul>
+          <li>Total Users: 1000</li>
+          <li>Total Orders: 500</li>
+          <li>Revenue: $50,000</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+

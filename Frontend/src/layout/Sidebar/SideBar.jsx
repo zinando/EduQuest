4253885d8592ab94603913e34aaef4502f971ac6
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-03 13:29:34 +0100

    Update SideBar.jsx

--- Frontend/src/layout/Sidebar/SideBar.jsx
+++ Frontend/src/layout/Sidebar/SideBar.jsx
@@ -2,49 +2,67 @@ import './SideBar.css'
 import * as Unicons from '@iconscout/react-unicons';
 import { Link } from 'react-router-dom';
 
-
-
 export default function SideBar() {
   return (
-
     <>
       <nav>
         <ul>
           <li className="logo">
-            <Link to="/SignUp" ><h2>E<span className='quest'>Q</span><span className='yellow'>.</span></h2></Link>
+            <Link to="/Signup" className='mb-3 logo'><h2 className='logo'>Edu<span>Quest</span></h2></Link>
           </li>
-          <li>
-            <Link to="/Home" className='active'><Unicons.UilCreateDashboard className="icon" />Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/Schedule"><Unicons.UilSchedule className="icon" />Schedule</Link>
-          </li>
-          <li>
-            <Link to="/subject"><Unicons.UilBooks className="icon" />Subjects</Link>
-          </li>
-          <li>
-            <Link to="/Exam"><Unicons.UilFile className="icon" />Exam</Link>
-          </li>
-          <li>
-            <Link to="/Support"><Unicons.UilCommentsAlt className="icon" />Support</Link>
-          </li>
-          <li>
-            <Link to="/Settings"><Unicons.UilSetting className="icon" />Settings</Link>
-          </li>
-        </ul>
-      </nav>
-      <div className="wrapper">
-        <div className="section">
-          <div className="box-area">
-            
+          <div className='links'>
+            <li>
+              <Link to="/Home" className='active nav-link'>
+                <Unicons.UilCreateDashboard className="icon" />
+                Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link to="/Schedule" className='nav-link'>
+                <Unicons.UilSchedule className="icon" />
+                Schedule
+              </Link>
+            </li>
+            <li>
+              <Link to="/Subject" className='nav-link'>
+                <Unicons.UilBooks className="icon" />
+                Subjects
+              </Link>
+            </li>
+            <li>
+              <Link to="/Exam" className='nav-link'>
+                <Unicons.UilFile className="icon" />
+                Exam
+              </Link>
+            </li>
+            <li>
+              <Link to="/Support" className='nav-link'>
+                <Unicons.UilCommentsAlt className="icon" />
+                Support
+              </Link>
+            </li>
+            <li>
+              <Link to="/Settings" className='nav-link'>
+                <Unicons.UilSetting className="icon" />
+                Settings
+              </Link>
+            </li>
             
           </div>
+        </ul>
+        
+      </nav>
+      
+      <div className="wrapper">
+        <div className="section">
+          <div className="box-area"></div>
         </div>
       </div>
+
+
+     
+     
+     
     </>
-
-
-
-
   )
 }

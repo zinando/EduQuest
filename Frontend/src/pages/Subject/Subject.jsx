40b65bc82f82a7a1de311980322fc38009ffe3ec
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-07 18:34:41 +0100

    Update Subject.jsx

--- Frontend/src/pages/Subject/Subject.jsx
+++ Frontend/src/pages/Subject/Subject.jsx
@@ -1,110 +1,64 @@
-import SideBar from '../../layout/Sidebar/SideBar';
-import './Subject.css'
-import { Container, Row, Col } from 'react-bootstrap';
-import { useState } from 'react';
-import Card from 'react-bootstrap/Card';
-import Book from '../../assets/book.jpeg';
+import '../../layout/Sidebar/Sidebar.css'
+import Navbar from '../../layout/NavBar/NavBar'
+import Sidebar from '../../layout/Sidebar/SideBar'
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
-export default function Schedule() {
-  // State to track the selected button
-  const [selectedButton, setSelectedButton] = useState(null);
 
-  // Define an array of button labels
-  const buttonLabels = ['Day', 'Status', 'Subject'];
-
-  // Function to handle button click
-  const handleButtonClick = (index) => {
-    setSelectedButton(index);
-  };
-
-  const cardStyle = {
-    width: '100%', // Set width to 100% for responsiveness
-    borderRadius: '15px',
-  };
-
-  const cardImageStyle = {
-    width: '100%', // Set width to 100% to fill the card width
-    height: '100%', // Set height to 100% to fill the card height
-    objectFit: 'cover',
-  };
-
+export default function Subject() {
   return (
     <>
-      <div className="schedule-container">
-        <SideBar />
-        <Container>
-          <Row>
-            <Col md={12}>
-              <h1 className="home-header pt-5">Schedule</h1>
-              <div className='pb-3 pt-3 group-btn'>
-                {buttonLabels.map((label, index) => (
-                  <button
-                    key={index}
-                    onClick={() => handleButtonClick(index)}
-                    className={selectedButton === index ? 'selected' : ''}
-                  >
-                    {label}
-                  </button>
-                ))}
+      <Sidebar />
+      <section className="home-section">
+        <Navbar />
+
+        <div className="home-content">
+          <h3 className='ps-3'>Subject</h3>
+          <ButtonGroup className="mb-3 btn-group" size="sm">
+            <Button className='btn' >Left</Button>
+            <Button className='btn' >Middle</Button>
+            <Button className='btn' >Right</Button>
+          </ButtonGroup>
+          <div className="box-card">
+            <div className="recent box">
+              <div className="title">Stats</div>
+              <div className="sdetails">
+                <ul className="details">
+                  <li className="topic">Date</li>
+                  <li>
+                    <a href="/">02 Jan 2023</a>
+                  </li>
+
+
+
+                </ul>
+                <ul className="details">
+                  <li className="topic">Name</li>
+                  <li>
+                    <a href="/">Alex Doe</a>
+                  </li>
+                </ul>
+                <ul className="details">
+                  <li className="topic">Grade</li>
+                  <li>
+                    <a href="/">Passed</a>
+                  </li>
+                </ul>
+                <ul className="details">
+                  <li className="topic">Teacher</li>
+                  <li>
+                    <a href="/">Peace</a>
+                  </li>
+                </ul>
               </div>
-            </Col>
-          </Row>
+            </div>
+          </div>
+        </div>
 
-          <Row>
-            <Col md={5}>
-              <Card style={cardStyle}>
-                <Row>
-                  <Col md={4}>
-                    <Card.Img
-                      variant="top"
-                      src={Book}
-                      style={cardImageStyle}
-                    />
-                  </Col>
-                  <Col md={8}>
-                    <Card.Body>
-                      <Card.Title>Geography</Card.Title>
-                      <Card.Text>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim vestibulum dui, at suscipit lacus varius in.
-                      </Card.Text>
-                    </Card.Body>
-                    <Card.Body>
-                      <Card.Link href="#">Card Link</Card.Link>
-                      <Card.Link href="#">Another Link</Card.Link>
-                    </Card.Body>
-                  </Col>
-                </Row>
-              </Card>
-            </Col>
 
-            <Col md={5}>
-              <Card style={cardStyle}>
-                <Row>
-                  <Col md={4}>
-                    <Card.Img
-                      variant="top"
-                      src={Book}
-                      style={cardImageStyle}
-                    />
-                  </Col>
-                  <Col md={8}>
-                    <Card.Body>
-                      <Card.Title>Geography</Card.Title>
-                      <Card.Text>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dignissim vestibulum dui, at suscipit lacus varius in.
-                      </Card.Text>
-                    </Card.Body>
-                    <Card.Body>
-                      <Card.Link href="#">Card Link</Card.Link>
-                      <Card.Link href="#">Another Link</Card.Link>
-                    </Card.Body>
-                  </Col>
-                </Row>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      </section>
+
     </>
-  );
+  )
 }
+

878c081a8da13d295114c592551259264e1d7617
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-10 23:00:00 +0100

    Update SignUp.jsx

--- Frontend/src/pages/Signup/SignUp.jsx
+++ Frontend/src/pages/Signup/SignUp.jsx
@@ -1,9 +1,40 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './SignUp.css';
-import * as Unicons from '@iconscout/react-unicons';
-import { Link } from 'react-router-dom';
+import * as Unicons from '@iconscout/react-unicons'
+import { Link } from 'react-router-dom'
+import queryBackEnd from '../queryBackEnd'
+
 
 export default function SignUp() {
+  // Function to handle form submission
+  const signUp = async (event) => {
+    event.preventDefault();
+
+    // Get form data
+    const formData = {
+      first_name: document.getElementById('first_name').value,
+      surname: document.getElementById('surname').value,
+      email: document.getElementById('email').value,
+      other_names: document.getElementById('other_names').value,
+      password: document.getElementById('password').value,
+    };
+
+    // Make a request to the backend
+    try {
+      const response = await queryBackEnd('http://localhost:5000', 'register', formData);
+      if (response.status === 1) {
+        alert('Registration successful!');
+      } else {
+        alert('Registration failed. Please try again.');
+      }
+    } catch (error) {
+      alert('An error occurred. Please try again later.');
+    }
+  };
+
+
+
+
   return (
     <>
       <div className="container-fluid content">
@@ -12,7 +43,7 @@ export default function SignUp() {
 
           <div className="col-lg-6 col-md-6 col-sm-12 form-container">
             <Link to="/" className='mb-3 logo'><h2 className='logo'>Edu<span className='quest'>Quest</span></h2></Link>
-            <form action="#">
+            <form onSubmit={signUp}>
               <div className="form-group mb-3 d-flex align-items-center">
                 <label htmlFor="name" className='form2'>
                   <Unicons.UilUser color="#0B88B3" size="25" />
@@ -22,6 +53,7 @@ export default function SignUp() {
                   className="form-control"
                   placeholder="Enter first name"
                   required=""
+                  id="first_name"
                 />
               </div>
               <div className="form-group mb-3 d-flex align-items-center">
@@ -33,6 +65,20 @@ export default function SignUp() {
                   className="form-control"
                   placeholder="Enter your surname"
                   required=""
+                  id="surname"
+                />
+              </div>
+
+              <div className="form-group mb-3 d-flex align-items-center">
+                <label htmlFor="name" className='form2'>
+                  <Unicons.UilBookReader color="#0B88B3" size="25" />
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Other names"
+                  required=""
+                  id="other_names"
                 />
               </div>
 
@@ -45,31 +91,11 @@ export default function SignUp() {
                   className="form-control"
                   placeholder="Enter your email"
                   required=""
+                  id="email"
                 />
               </div>
-              <div className="form-group mb-3 d-flex align-items-center">
-                <label htmlFor="name" className='form2'>
-                  <Unicons.UilBookReader color="#0B88B3" size="25" />
-                </label>
-                <input
-                  type="password"
-                  className="form-control"
-                  placeholder="Create Username"
-                  required=""
-                />
-              </div>
-              <div className="form-group mb-3 d-flex align-items-center">
-                <label htmlFor="name" className='form2'>
-                  <Unicons.UilBookReader color="#0B88B3" size="25" />
-                </label>
-                <select id="class" name="class" className="form-control" required="">
-                  <option value="">Select a class</option>
-                  <option value="class1">Class 1</option>
-                  <option value="class2">Class 2</option>
-                  <option value="class3">Class 3</option>
-                </select>
 
-              </div>
+
 
               <div className="form-group mb-3 d-flex align-items-center">
                 <label htmlFor="name" className='form2'>
@@ -80,17 +106,7 @@ export default function SignUp() {
                   className="form-control"
                   placeholder="Create password"
                   required=""
-                />
-              </div>
-              <div className="form-group mb-3 d-flex align-items-center">
-                <label htmlFor="name" className='form2'>
-                  <Unicons.UilLock color="#0B88B3" size="25" />
-                </label>
-                <input
-                  type="password"
-                  className="form-control"
-                  placeholder="Confirm password"
-                  required=""
+                  id="password"
                 />
               </div>
 
@@ -101,14 +117,13 @@ export default function SignUp() {
                 </label>
               </div>
               <div className='mb-4 d-grid'>
-                <Link to="/Home" className='btn button'>Register</Link>
+                <button type="submit" className='btn button'>Register</button>
               </div>
               <div className="text">
                 <h3>
                   Already have an account? <Link to="/Login">Login</Link>
                 </h3>
               </div>
-
             </form>
           </div>
         </div>

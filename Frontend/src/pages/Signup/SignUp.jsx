5f888fff5550ffd85022e31192499f89b2a71941
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-10 14:53:17 +0100

    fixed home link

--- Frontend/src/pages/Signup/SignUp.jsx
+++ Frontend/src/pages/Signup/SignUp.jsx
@@ -11,7 +11,7 @@ export default function SignUp() {
           <div className="col-lg-6 col-md-6 d-none d-md-block image-container"></div>
 
           <div className="col-lg-6 col-md-6 col-sm-12 form-container">
-            <Link to="/SignUp" className='mb-3 logo'><h2 className='logo'>Edu<span className='quest'>Quest</span></h2></Link>
+            <Link to="/" className='mb-3 logo'><h2 className='logo'>Edu<span className='quest'>Quest</span></h2></Link>
             <form action="#">
               <div className="form-group mb-3 d-flex align-items-center">
                 <label htmlFor="name" className='form2'>

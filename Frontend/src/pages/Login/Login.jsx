4558da7c1a1148dba01a28a2106cf8607f02f737
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-17 20:38:09 +0100

    Update Login.jsx

--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
+
 import Form from 'react-bootstrap/Form';
 import * as Unicons from '@iconscout/react-unicons'
-import './Login.css';
+import '../../pages/Signup/SignUp.css'
 import queryBackEnd, { setSession, userInfo } from '../queryBackEnd'
 
 
@@ -31,7 +31,7 @@ export default function Login() {
       const userData = {
         userid,
         password,
-      };      
+      };
 
       // Send a POST request to your Flask backend using queryBackEnd
       const response = await queryBackEnd('/login', userData);
@@ -40,18 +40,17 @@ export default function Login() {
       if (response.status === 1) {
         // Login successful, create session and store user data
         setSession(response);
-        if (userInfo().adminType === 'super'){
+        if (userInfo().adminType === 'super') {
           // redirect to super admin dashboard
-          location.href= '/home';
-        }        
-        
+          location.href = '/home';
+        }
+
       } else {
         // Login failed, display error to user and log on console
         var msg = "";
-        for (var i = 0; i < response.error.length; i++)
-          {msg += response.error[i] + '\n'; }
+        for (var i = 0; i < response.error.length; i++) { msg += response.error[i] + '\n'; }
         setResp(msg);
-        console.error('Login failed:', response.message,'\n'+msg);
+        console.error('Login failed:', response.message, '\n' + msg);
       }
     }
   };
@@ -61,17 +60,17 @@ export default function Login() {
     <>
       <div className="container-fluid content">
         <div className='row'>
-          <div className="col-lg-6 col-md-6 d-none d-md-block image-container"></div>
+          <div className="col-lg-6 col-md-6 d-none d-md-block image-container image-fluid"></div>
 
           <div className="col-lg-6 col-md-6 col-sm-12 form-container">
             <div className='row'>
-              <div 
-                  className="col-12 text-center"
-                  style={{
-                      color:'#E97464',
-                      marginBottom:'12px'
-                  }}>{resp}
-              </div> 
+              <div
+                className="col-12 text-center"
+                style={{
+                  color: '#E97464',
+                  marginBottom: '12px'
+                }}>{resp}
+              </div>
             </div>
             <Link to="/" className='mb-3 logo'><h2 className='logo'>Edu<span className='quest'>Quest</span></h2></Link>
             <Form noValidate validated={validated} onSubmit={handleSubmit}>
@@ -79,53 +78,48 @@ export default function Login() {
                 <label htmlFor="name" className='form2'>
                   <Unicons.UilUser color="#0B88B3" size="25" />
                 </label>
-                <Form.Group as={Col} md="8" controlId="validationCustom01">
-
-                  <Form.Control
-                    className="form-control"
-                    required={true}
-                    name='userid'
-                    type="text"
-                    placeholder="Enter userid or email"
-                    style={{marginLeft: '12px'}}
-                  
-                  />
-                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                </Form.Group>
+                <input
+                  className="form-control"
+                  required={true}
+                  name='userid'
+                  type="text"
+                  placeholder="Enter userid or email"
+                  style={{ marginLeft: '12px' }}
+                />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
               </div>
+
               <div className="form-group mb-3 d-flex align-items-center">
                 <label htmlFor="name" className='form2'>
-                  <Unicons.UilLock color="#0B88B3" size="25" />
+                  <Unicons.UilUser color="#0B88B3" size="25" />
                 </label>
-                <Form.Group as={Col} md="8" controlId="validationCustom02">
-                  
-                  <Form.Control
-                    required={true}
-                    name='password'
-                    type="password"
-                    placeholder="Enter password"
-                    style={{marginLeft: '12px'}}
-                    
-                  />
-                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                </Form.Group>
+                <input
+                  className="form-control"
+                  required={true}
+                  name='password'
+                  type="password"
+                  placeholder="Enter password"
+                  style={{ marginLeft: '12px' }}
+                />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
               </div>
     
               <div className="form-check">
-                <Form.Group className="mb-3">
-                  <Form.Check
-                    required=""
-                    name='rememberMe'
-                    label="Remember me"                    
-                  />
-                </Form.Group>
+                <input type="checkbox" required={true} className="form-check-input" />
+                <label className="form-check-label">
+                  Remember me
+                </label>
               </div>
-              <Button type="submit" className='btn button'>Login</Button>
+
+          
               <div className='mb-4 d-grid'>
-              <div className="text">
-                <h3>
-                  Don&apos;t have an account? <Link to="/Signup">SignUp</Link>
-                </h3>
+                <button type="submit" className='btn button'>Login</button>
+              </div>
+              <div className='mb-4 d-grid'>
+                <div className="text">
+                  <h3>
+                    Don&apos;t have an account? <Link to="/Signup">SignUp</Link>
+                  </h3>
                 </div>
               </div>
             </Form>

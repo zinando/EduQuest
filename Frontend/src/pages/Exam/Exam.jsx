ceadf5600de30c1feb55f5e563a23352df473090
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-18 11:51:24 +0100

    Update Exam.jsx

--- Frontend/src/pages/Exam/Exam.jsx
+++ Frontend/src/pages/Exam/Exam.jsx
@@ -1,7 +1,8 @@
-import ListGroup from 'react-bootstrap/ListGroup';
 import '../../layout/Sidebar/SideBar.css'
 import Sidebar from '../../layout/Sidebar/SideBar'
 import Navbar from '../../layout/Navbar/NavBar'
+
+
 
 
 export default function Exam() {
@@ -12,17 +13,9 @@ export default function Exam() {
       <section className="home-section">
         <Navbar />
         <div className="home-content">
-          <ListGroup as="ol" numbered>
-            <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
-            <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
-            <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
-            <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
-            <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
-            <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
-            <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
-            <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
+         
+       
 
-          </ListGroup>
 
           
         </div>

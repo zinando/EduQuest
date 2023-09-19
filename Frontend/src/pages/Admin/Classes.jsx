542949e68660f3770b8eb6a0558b6af9dcdf218c
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-19 11:12:58 +0100

    Create Classes.jsx

---
+++ Frontend/src/pages/Admin/Classes.jsx
@@ -0,0 +1,23 @@
+import '../../layout/Sidebar/SideBar.css'
+import Sidebar from '../../layout/Sidebar/SideBar'
+import Navbar from '../../layout/Navbar/NavBar'
+import Calendar from '../../component/Calendar/Calendar'
+
+
+
+
+export default function Classes() {
+  return (
+    <>
+      <Sidebar />
+
+      <section className="home-section">
+        <Navbar />
+        <div className="home-content">
+          <Calendar />
+        </div>
+      </section>
+    </>
+  )
+}
+

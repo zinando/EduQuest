035e8c8d689d2126b1d4c1d4cdfcff19d0be3b49
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-19 13:26:52 +0100

    Updated Users management

--- Frontend/src/pages/Admin/Users.jsx
+++ Frontend/src/pages/Admin/Users.jsx
@@ -1,15 +1,62 @@
+import { useState } from 'react';
 import 'react-calendar/dist/Calendar.css';
 import '../Schedule/Schedule.css'
 import '../../layout/Sidebar/SideBar.css';
 import Navbar from '../../layout/NavBar/NavBar';
 import Sidebar from '../../layout/Sidebar/SideBar';
-import Table from 'react-bootstrap/Table';
-import Container from 'react-bootstrap/Container';
-import { Col, Row } from 'react-bootstrap';
-
+import { Button, Table, Modal, Form } from 'react-bootstrap';
 
 export default function Subject() {
+  const [users, setUsers] = useState([
+    { id: 1, name: 'User 1', email: 'user1@example.com' },
+    { id: 2, name: 'User 2', email: 'user2@example.com' },
+    { id: 3, name: 'User 3', email: 'user3@example.com' },
+  ]);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [newUser, setNewUser] = useState({ name: '', email: '' });
 
+  const handleEditUser = (user) => {
+    setSelectedUser(user);
+    setShowEditModal(true);
+  };
+
+  const handleCloseEditModal = () => {
+    setSelectedUser(null);
+    setShowEditModal(false);
+  };
+
+  const handleUpdateUser = (updatedUser) => {
+    const updatedUsers = users.map((user) =>
+      user.id === updatedUser.id ? updatedUser : user
+    );
+    setUsers(updatedUsers);
+    handleCloseEditModal();
+  };
+
+  const handleDeleteUser = (userId) => {
+    const updatedUsers = users.filter((user) => user.id !== userId);
+    setUsers(updatedUsers);
+  };
+
+  const handleShowAddModal = () => {
+    setShowAddModal(true);
+  };
+
+  const handleCloseAddModal = () => {
+    setShowAddModal(false);
+    setNewUser({ name: '', email: '' });
+  };
+
+  const handleAddUser = () => {
+    const newUserWithId = {
+      ...newUser,
+      id: users.length + 1, // Generate a new ID remember ohhh
+    };
+    setUsers([...users, newUserWithId]);
+    handleCloseAddModal();
+  };
 
   return (
     <>
@@ -17,82 +64,115 @@ export default function Subject() {
       <section className="home-section">
         <Navbar />
         <div className="home-content">
-          <Container>
-            <Row>
-              <Col xs={8}>
-                <div className='border-0 shadow rounded-0 p-4' style={{ height: '300px', overflowY: 'scroll' }}>
-                  <h4 className="card-title fw-bold">Schedule</h4>
-                  <Table striped bordered hover>
-                    <thead>
-                      <tr>
-                        <th>Date</th>
-                        <th>Time</th>
-                        <th>Subject</th>
-                        <th>Class</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>16.10.23</td>
-                        <td>8:00am</td>
-                        <td>English Language</td>
-                        <td>JSS 1</td>
-                      </tr>
-                      <tr>
-                        <td>18.10.23</td>
-                        <td>10:30am</td>
-                        <td>Mathematics</td>
-                        <td>JSS 1</td>
-                      </tr>
-                      <tr>
-                        <td>19.10.23</td>
-                        <td>8:00am</td>
-                        <td>French</td>
-                        <td>JSS 1</td>
-                      </tr>
-                      <tr>
-                        <td>20.10.23</td>
-                        <td>8:00am</td>
-                        <td>Geography</td>
-                        <td>JSS 1</td>
-                      </tr>
-                      <tr>
-                        <td>20.10.23</td>
-                        <td>8:00am</td>
-                        <td>Geography</td>
-                        <td>JSS 1</td>
-                      </tr>
-                      <tr>
-                        <td>20.10.23</td>
-                        <td>8:00am</td>
-                        <td>Geography</td>
-                        <td>JSS 1</td>
-                      </tr>
-                      <tr>
-                        <td>19.10.23</td>
-                        <td>8:00am</td>
-                        <td>French</td>
-                        <td>JSS 1</td>
-                      </tr>
-                      <tr>
-                        <td>19.10.23</td>
-                        <td>8:00am</td>
-                        <td>French</td>
-                        <td>JSS 1</td>
-                      </tr>
-                      <tr>
-                        <td>19.10.23</td>
-                        <td>8:00am</td>
-                        <td>French</td>
-                        <td>JSS 1</td>
-                      </tr>
-                    </tbody>
-                  </Table>
+          <div>
+            <h1>Manage Users</h1>
+            <Button variant="primary" onClick={handleShowAddModal}>
+              Add User
+            </Button>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user) => (
+                  <tr key={user.id}>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                      <Button variant="primary" onClick={() => handleEditUser(user)}>
+                        Edit
+                      </Button>{' '}
+                      <Button variant="danger" onClick={() => handleDeleteUser(user.id)}>
+                        Delete
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
 
-                </div>
-              </Col>
-            </Row>
-          </Container>
+            {/* Edit User Modal */}
+            <Modal show={showEditModal} onHide={handleCloseEditModal}>
+              <Modal.Header closeButton>
+                <Modal.Title>Edit User</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form>
+                  <Form.Group controlId="formBasicName">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter name"
+                      value={selectedUser?.name || ''}
+                      onChange={(e) =>
+                        setSelectedUser({ ...selectedUser, name: e.target.value })
+                      }
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter email"
+                      value={selectedUser?.email || ''}
+                      onChange={(e) =>
+                        setSelectedUser({ ...selectedUser, email: e.target.value })
+                      }
+                    />
+                  </Form.Group>
+                </Form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleCloseEditModal}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={() => handleUpdateUser(selectedUser)}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
+
+            {/* Add User Modal */}
+            <Modal show={showAddModal} onHide={handleCloseAddModal}>
+              <Modal.Header closeButton>
+                <Modal.Title>Add User</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form>
+                  <Form.Group controlId="formBasicName">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter name"
+                      value={newUser.name}
+                      onChange={(e) => setNewUser({ ...newUser, name: e.target.value })}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter email"
+                      value={newUser.email}
+                      onChange={(e) => setNewUser({ ...newUser, email: e.target.value })}
+                    />
+                  </Form.Group>
+                </Form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleCloseAddModal}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={handleAddUser}>
+                  Add User
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
         </div>
       </section>
     </>

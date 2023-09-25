22218b37f60e48ad7f273265d21966767d1d55ac
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-25 12:49:22 +0100

    Update Login.jsx

--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -1,15 +1,35 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
-import * as Unicons from '@iconscout/react-unicons'
-import '../Signup/SignUp.css'
-import queryBackEnd, { setSession, userInfo } from '../queryBackEnd'
-
+import * as Unicons from '@iconscout/react-unicons';
+import '../Signup/SignUp.css';
+import queryBackEnd, { setSession, userInfo } from '../queryBackEnd';
+import Swal from 'sweetalert2';
 
 export default function Login() {
   const [validated, setValidated] = useState(false);
   const [resp, setResp] = useState('');
   const [passwordVisible, setPasswordVisible] = useState(false);
+
+  // Function to show SweetAlert2 toast notification
+  const showToast = () => {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer);
+        toast.addEventListener('mouseleave', Swal.resumeTimer);
+      },
+    });
+
+    Toast.fire({
+      icon: 'success',
+      title: 'Signed in successfully',
+    });
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -41,9 +61,12 @@ export default function Login() {
           // redirect to super admin dashboard
           location.href = '/home';
         }
+
+        // Show success toast notification
+        showToast();
       } else {
         // Login failed, display error to user and log on console
-        var msg = "";
+        var msg = '';
         for (var i = 0; i < response.error.length; i++) {
           msg += response.error[i] + '\n';
         }
@@ -56,31 +79,29 @@ export default function Login() {
   return (
     <>
       <div className="container-fluid content">
-        <div className='row'>
+        <div className="row">
           <div className="col-lg-6 col-md-6 d-none d-md-block image-container image-fluid"></div>
 
-          <div className="col-lg-6 col-md-6 col-sm-12 form-container" style={{
-            paddingBottom: '200px'
-          }}>
-            <div className='row'>
-              <div
-                className="col-12 text-center"
-                style={{
-                  color: '#E97464',
-                  marginBottom: '12px'
-                }}>{resp}
+          <div className="col-lg-6 col-md-6 col-sm-12 form-container mx-auto" style={{ paddingBottom: '200px' }}>
+            <div className="row">
+              <div className="col-12 text-center" style={{ color: '#E97464', marginBottom: '12px' }}>
+                {resp}
               </div>
             </div>
-            <Link to="/" className='mb-3 logo'><h2 className='logo'>Edu<span className='quest'>Quest</span></h2></Link>
+            <Link to="/" className="mb-3 logo">
+              <h2 className="logo">
+                Edu<span className="quest">Quest</span>
+              </h2>
+            </Link>
             <Form noValidate validated={validated} onSubmit={handleSubmit}>
               <div className="form-group mb-3 d-flex align-items-center">
-                <label htmlFor="name" className='form2'>
+                <label htmlFor="name" className="form2">
                   <Unicons.UilUser color="#0B88B3" size="25" />
                 </label>
                 <input
                   className="form-control"
                   required={true}
-                  name='userid'
+                  name="userid"
                   type="text"
                   placeholder="Enter userid or email"
                   style={{ marginLeft: '12px' }}
@@ -89,14 +110,14 @@ export default function Login() {
               </div>
 
               <div className="form-group mb-3 pt-3 d-flex align-items-center">
-                <label htmlFor="name" className='form2'>
+                <label htmlFor="name" className="form2">
                   <Unicons.UilUser color="#0B88B3" size="25" />
                 </label>
                 <input
                   className="form-control"
                   required={true}
-                  name='password'
-                  type={passwordVisible ? "text" : "password"}
+                  name="password"
+                  type={passwordVisible ? 'text' : 'password'}
                   placeholder="Enter password"
                   style={{ marginLeft: '12px' }}
                 />
@@ -108,15 +129,15 @@ export default function Login() {
 
               <div className="form-check">
                 <input type="checkbox" required={true} className="form-check-input" />
-                <label className="form-check-label">
-                  Remember me
-                </label>
+                <label className="form-check-label">Remember me</label>
               </div>
 
-              <div className='mb-4 d-grid'>
-                <button type="submit" className='btn button'>Login</button>
+              <div className="mb-4 d-grid">
+                <button type="submit" className="btn button">
+                  Login
+                </button>
               </div>
-              <div className='mb-4 d-grid'>
+              <div className="mb-4 d-grid">
                 <div className="text">
                   <h3>
                     Don&apos;t have an account? <Link to="/Signup">SignUp</Link>

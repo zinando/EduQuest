8f171b77206210c61d7ba7817ee9db3281a40919
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-26 21:34:21 +0100

    Update Exam.jsx

--- Frontend/src/pages/Exam/Exam.jsx
+++ Frontend/src/pages/Exam/Exam.jsx
@@ -1,27 +1,58 @@
 import '../../layout/Sidebar/SideBar.css'
 import Sidebar from '../../layout/Sidebar/SideBar'
 import Navbar from '../../layout/Navbar/NavBar'
-import Calendar from '../../component/Calendar/Calendar'
 
 
 
+const Exam = () => {
+  const questions = [
+    'What is the capital of France?',
+    'What is the largest ocean in the world?',
+    'What is the square root of 16?',
+    'What is the meaning of life?',
+    'What is the best programming language?'
+  ];
 
-export default function Exam() {
+  const answers = [
+    ['Paris', 'Lyon', 'Marseille'],
+    ['Pacific Ocean', 'Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean'],
+    ['4', '6', '8', '10'],
+    ['The meaning of life is up to each individual to decide.', 'To find happiness.', 'To make a difference in the world.'],
+    ['Python', 'JavaScript', 'Java', 'C++']
+  ];
+
   return (
     <>
       <Sidebar />
-
       <section className="home-section">
         <Navbar />
         <div className="home-content">
-         
-       <Calendar/>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Question</th>
+                <th>Answer</th>
+              </tr>
+            </thead>
+            <tbody>
+              {questions.map((question, index) => (
+                <tr key={index}>
+                  <td>{question}</td>
+                  <td>
+                    {answers[index].map((answer, index) => (
+                      <option key={index}>{answer}</option>
+                    ))}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
 
 
-          
         </div>
       </section>
     </>
   )
 }
+export default Exam
 

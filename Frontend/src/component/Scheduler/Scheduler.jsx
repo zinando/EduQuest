a24339114bb37a8ca1c14cb70d86528d5a926ae1
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-09-26 21:21:48 +0100

    Update Scheduler.jsx

--- Frontend/src/component/Scheduler/Scheduler.jsx
+++ Frontend/src/component/Scheduler/Scheduler.jsx
@@ -1,18 +1,25 @@
-import  { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Calendar } from '@fullcalendar/core';
 import listPlugin from '@fullcalendar/list';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const MyCalendar = () => {
   const calendarElRef = useRef(null);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [eventTitle, setEventTitle] = useState('');
+  const [eventStart, setEventStart] = useState('');
+  const [eventEnd, setEventEnd] = useState('');
 
   useEffect(() => {
     const calendarEl = calendarElRef.current;
 
     const calendar = new Calendar(calendarEl, {
       plugins: [listPlugin],
-      editable: true,
-      initialView: 'listWeek', 
-      events: [ 
+      initialView: 'listWeek',
+      events: [
         {
           title: 'Debate Show',
           start: '2023-09-26',
@@ -44,6 +51,7 @@ const MyCalendar = () => {
         center: '',
         right: 'listDay,listWeek,listMonth',
       },
+      eventClick: handleEventClick,
     });
 
     calendar.render();
@@ -53,8 +61,93 @@ const MyCalendar = () => {
     };
   }, []);
 
+  function handleEventClick(info) {
+    setSelectedEvent(info.event);
+    setEventTitle(info.event.title);
+    setEventStart(info.event.start.toISOString().substring(0, 16));
+    setEventEnd(info.event.end.toISOString().substring(0, 16));
+    setIsModalOpen(true);
+  }
+
+  function closeModal() {
+    setIsModalOpen(false);
+  }
+
+  function handleTitleChange(event) {
+    setEventTitle(event.target.value);
+  }
+
+  function handleStartChange(event) {
+    setEventStart(event.target.value);
+  }
+
+  function handleEndChange(event) {
+    setEventEnd(event.target.value);
+  }
+
+  function saveEvent() {
+    if (selectedEvent) {
+      // Edit existing event
+      selectedEvent.setProp('title', eventTitle);
+      selectedEvent.setStart(eventStart);
+      selectedEvent.setEnd(eventEnd);
+    } else {
+      // Add a new event
+      const newEvent = {
+        title: eventTitle,
+        start: eventStart,
+        end: eventEnd,
+      };
+      calendarElRef.current.getApi().addEvent(newEvent);
+    }
+    closeModal();
+  }
+
   return (
-    <div ref={calendarElRef} />
+    <div>
+      <div ref={calendarElRef} />
+      <Button variant="primary" onClick={() => setIsModalOpen(true)}>
+        Add Event
+      </Button>
+      <Modal show={isModalOpen} onHide={closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>{selectedEvent ? 'Edit Event' : 'Add Event'}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="eventTitle">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                value={eventTitle}
+                onChange={handleTitleChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="eventStart">
+              <Form.Label>Start Date & Time</Form.Label>
+              <Form.Control
+                type="datetime-local"
+                value={eventStart}
+                onChange={handleStartChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="eventEnd">
+              <Form.Label>End Date & Time</Form.Label>
+              <Form.Control
+                type="datetime-local"
+                value={eventEnd}
+                onChange={handleEndChange}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={saveEvent}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
   );
 };
 

8c3bf58af7b1036a2d53d584630f640bd299cce1
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-10-02 18:38:05 +0100

    Update Exam.jsx

--- Frontend/src/pages/Exam/Exam.jsx
+++ Frontend/src/pages/Exam/Exam.jsx
@@ -1,25 +1,23 @@
 import '../../layout/Sidebar/SideBar.css'
 import Sidebar from '../../layout/Sidebar/SideBar'
 import Navbar from '../../layout/Navbar/NavBar'
-
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+import CountdownTimer from '../../component/Timer/Timer';
+import './exam.css'
 
 
 const Exam = () => {
-  const questions = [
-    'What is the capital of France?',
-    'What is the largest ocean in the world?',
-    'What is the square root of 16?',
-    'What is the meaning of life?',
-    'What is the best programming language?'
-  ];
 
-  const answers = [
-    ['Paris', 'Lyon', 'Marseille'],
-    ['Pacific Ocean', 'Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean'],
-    ['4', '6', '8', '10'],
-    ['The meaning of life is up to each individual to decide.', 'To find happiness.', 'To make a difference in the world.'],
-    ['Python', 'JavaScript', 'Java', 'C++']
-  ];
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
   return (
     <>
@@ -27,26 +25,33 @@ const Exam = () => {
       <section className="home-section">
         <Navbar />
         <div className="home-content">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Question</th>
-                <th>Answer</th>
-              </tr>
-            </thead>
-            <tbody>
-              {questions.map((question, index) => (
-                <tr key={index}>
-                  <td>{question}</td>
-                  <td>
-                    {answers[index].map((answer, index) => (
-                      <option key={index}>{answer}</option>
-                    ))}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <div className='exam'>
+            <ul>
+              
+              <li><h5>Fist Term Examination</h5></li>
+              <li> <CountdownTimer /></li>
+              <li>Mathematics</li>
+              <li>Score: 5</li>
+            </ul>
+          </div>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+              <Grid xs={8}>
+                <Item>
+                  In a right triangle, if one of the legs is 3 units long, 
+                  and the hypotenuse is 5 units long, what is the length of the other leg?
+                </Item>
+              </Grid>
+              <Grid xs={4}>
+                <Item>xs=4</Item>
+              </Grid>
+             
+              <Grid xs={12}>
+                <Item>xs=8</Item>
+              </Grid>
+            </Grid>
+          </Box>
+          
 
 
         </div>

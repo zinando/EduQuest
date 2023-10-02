c976cc9613f980d1992e822e036d4ee4d21298ae
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-10-02 22:26:51 +0100

    Update Support.jsx

--- Frontend/src/pages/Support/Support.jsx
+++ Frontend/src/pages/Support/Support.jsx
@@ -1,70 +1,128 @@
-import { useEffect } from 'react';
-import echarts from 'echarts';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function PieChart() {
-  useEffect(() => {
-    // Create a DOM element to render the chart
-    const chartContainer = document.getElementById('pie-chart-container');
-    const chart = echarts.init(chartContainer);
-
-    // Define the data for the pie chart
-    const data = [
-      { name: 'Teachers', value: 12 },
-      { name: 'Students', value: 403 },
-      { name: 'Reviewers', value: 6 },
-    ];
-
-    // Create the pie chart option
-    const option = {
-      title: {
-        text: 'User Stats',
-        textStyle: {
-          fontWeight: 'bold',
-          fontSize: 16,
-        },
-      },
-      series: [
-        {
-          type: 'pie',
-          radius: '55%',
-          center: ['50%', '60%'],
-          data: data.map(item => ({
-            name: item.name,
-            value: item.value,
-          })),
-          itemStyle: {
-            emphasis: {
-              shadowBlur: 10,
-              shadowOffsetX: 0,
-              shadowColor: 'rgba(0, 0, 0, 0.5)',
-            },
-          },
-          label: {
-            show: true,
-            formatter: '{b}: {c} ({d}%)',
-          },
-        },
-      ],
-    };
-
-    // Set the chart option and resize the chart when the window size changes
-    chart.setOption(option);
-    window.addEventListener('resize', () => {
-      chart.resize();
-    });
-
-    // Clean up the chart when the component is unmounted
-    return () => {
-      chart.dispose();
-      window.removeEventListener('resize', () => {
-        chart.resize();
-      });
-    };
-  }, []);
-
+function Copyright(props) {
   return (
-    <div id="pie-chart-container" style={{ width: '100%', height: '300px' }}></div>
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
 
-export default PieChart;
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+export default function SignInSide() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}

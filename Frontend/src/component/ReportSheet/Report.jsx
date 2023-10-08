a136e24c15b2a5144a44766205e35ebec60ce7b0
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-10-08 11:12:06 +0100

    Update Report.jsx

--- Frontend/src/component/ReportSheet/Report.jsx
+++ Frontend/src/component/ReportSheet/Report.jsx
@@ -1,7 +1,11 @@
-import { Typography, Table, TableContainer, TableHead, TableRow, TableCell, TableBody, Paper } from '@mui/material';
+
+import { Typography, Table, TableContainer, TableHead, TableRow, TableCell, TableBody, Paper, Button } from '@mui/material';
+import { userInfo } from '../../pages/queryBackEnd';
 
 function App() {
-  const studentName = 'John Doe';
+  const user = userInfo(); // Get user information
+  const userFullName = user.fullName; //Get the full name from user information
+
   const courses = [
     { name: 'Math', score: 85 },
     { name: 'Science', score: 92 },
@@ -14,35 +18,52 @@ function App() {
   const totalScores = courses.reduce((total, course) => total + course.score, 0);
   const cumulativeAverage = (totalScores / courses.length).toFixed(2);
 
+  // Function to handle the "View Transcript" action
+  const handleViewTranscript = (courseName) => {
+    // Add custom logic for viewing the transcript(will use this in future)
+    console.log(`View Transcript for ${courseName}`);
+  };
+
   return (
     <div className="home-content pt-2 p-4">
-      <Typography variant="h5" gutterBottom>
+      <Typography variant="h6" gutterBottom>
         Report Sheet
       </Typography>
-      <Typography variant="h5" gutterBottom>
-        Student Name: {studentName}
+      <Typography variant="h6" gutterBottom>
+        Student Name: {userFullName}
+      </Typography>
+      <Typography variant="h6" gutterBottom style={{ color: '#0B88B3' }}>
+        Cumulative Average: {cumulativeAverage}
       </Typography>
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Course</TableCell>
-              <TableCell>Score</TableCell>
+              <TableCell style={{ width: '33%' }}>Course</TableCell>
+              <TableCell style={{ width: '33%' }}>Score</TableCell>
+              <TableCell style={{ width: '34%' }}>Action</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {courses.map((course, index) => (
               <TableRow key={index}>
-                <TableCell>{course.name}</TableCell>
-                <TableCell>{course.score}</TableCell>
+                <TableCell style={{ width: '33%' }}>{course.name}</TableCell>
+                <TableCell style={{ width: '33%' }}>{course.score}</TableCell>
+                <TableCell style={{ width: '34%' }}>
+                  <Button
+                    variant="outlined"
+                    onClick={() => handleViewTranscript(course.name)}
+                    style={{ marginLeft: 'auto' }} 
+                  >
+                    View Transcript
+                  </Button>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
-      <Typography variant="h6" gutterBottom>
-        Cumulative Average: {cumulativeAverage}
-      </Typography>
+      
     </div>
   );
 }
